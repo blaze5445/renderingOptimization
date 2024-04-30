85a1802f6016ef0094ed517dd057312f97e5d918
blaze5445 <blaze5445@tutamail.com>
2024-04-30 18:29:29 +0300

    update configs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace FermatP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equacao Teorema = new Equacao(3, 6, 9);
+            Byte looping = 1;
+            do
+            {
+                Teorema.SetExpoente(looping);
+                Console.WriteLine(Teorema);
+                looping += 1;
+            } while (looping < Byte.MaxValue);
+        }
+    }
+}
