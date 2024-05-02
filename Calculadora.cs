680694270880ce5bfcdebcf7787b9e5231897575
blaze5445 <blaze5445@tutamail.com>
2024-05-02 18:29:29 +0300

    Fixed alignment issues

---
+++ Calculadora.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FermatP
+{
+    static class Calculadora
+    {
+        //Próximo Passo: Criar overrides para aceitar outros tipos de inteiros como int16, int32...
+        public static ulong somarPotencias(List<ulong> listaPotencias)
+        {
+            ulong somaDasPotencias = 0;
+            foreach(ulong potencia in listaPotencias)
+            {
+                somaDasPotencias += potencia;
+            }
+            return somaDasPotencias;
+        }
+
+        public static string somarAlgarismos(char[] algarismos)
+        {
+            int somaDosAlgarismos = 0;
+
+            foreach(char c in algarismos)
+            {
+                somaDosAlgarismos += (int)Char.GetNumericValue(c);
+            }
+            
+            return "" + somaDosAlgarismos;
+        }
+
+        public static ulong multiplicar(ulong _potencia, Byte _base)
+        {
+            return _potencia * _base;
+        }
+    }
+}
